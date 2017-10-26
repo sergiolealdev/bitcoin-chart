c52c756ae636f80fce1cb369ecd5f03d382ee0af
Sergio Leal <sergio@sergiolealdev.com>
2017-10-26 21:53:45 +0100

    adding bitcoin api and testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {Http} from '@angular/http';
 import 'rxjs/add/operator/map';
+
+import * as Rx from 'rxjs/Rx';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,14 +13,31 @@ export class AppComponent implements OnInit{
   title = 'app';
   isLoading:boolean;
   data:any;
+  value:any;
   constructor(private http: Http) {
 
   }
 
   ngOnInit() {
+  var source = Rx.Observable
+      .interval(500 /* ms */)
+      .timeInterval();
+
+  var subscription = source.subscribe(
+      function (x) {
+          console.log('Next: ' + x.toString());
+          console.log(typeof x);
+          this.value = x;
+      },
+      function (err) {
+          console.log('Error: ' + err);
+      },
+      function () {
+          console.log('Completed');
+      });
     console.log("Entramos");
     this.isLoading=true;
-    this.http.get('https://s2k7tnzlhrpw.statuspage.io/api/v1/summary.json')
+    this.http.get('https://api.coindesk.com/v1/bpi/currentprice.json')
       .map(res => res.json())
       .subscribe(
         (data) => {
