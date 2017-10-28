bf462673c6b72283c20a8ebd0a890e3c4e134b28
Sergio Leal <sergio@sergiolealdev.com>
2017-10-28 10:02:44 +0100

    adding variation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-import {Http} from '@angular/http';
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 import * as Rx from 'rxjs/Rx';
@@ -12,19 +12,22 @@ import * as moment from 'moment'; // add this 1 of 4
 })
 export class AppComponent implements OnInit {
 
+  arrayValues: any[] = [];
+  currentValue:any;
+  variation:any;
   constructor(private http: Http) {
 
   }
 
   ngOnInit() {
     let now = moment(); // add this 2 of 4
-    let url = 'https://api.coindesk.com/v1/bpi/historical/close.json?start=' + now.subtract(30,'days') + '&end=' 
-    + now;
+    let url = 'https://api.coindesk.com/v1/bpi/historical/close.json?start=' + now.subtract(30, 'days') + '&end='
+      + now;
     let url2 = 'https://api.coindesk.com/v1/bpi/historical/close.json?start=2017-10-01&end=2017-10-26';
     console.log(url);
     this.http.get(url2)
-    .map(res => res.json())
-    .subscribe(
+      .map(res => res.json())
+      .subscribe(
       (data) => {
         /*const _lineChartData: Array<any> = new Array(val.value + 1);
         arrayValues.push(parseFloat(data.bpi.USD.rate.replace(",", "")));
@@ -37,37 +40,52 @@ export class AppComponent implements OnInit {
 
         this.lineChartLabels.push(date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds());
 */
-console.log("££££££££££££££££££££££££££££££££££££££££££££££££££££££");
-console.log(data.bpi);
+        console.log("££££££££££££££££££££££££££££££££££££££££££££££££££££££");
+        console.log(data.bpi);
       }
-    );
+      );
 
     const source = Rx.Observable
       .interval(2 * 1000 /* ms */)
       .timeInterval();
 
-    const arrayValues: any[] = [];
-    
     const subscription = source.subscribe(val => {
 
       this.http.get('https://api.coindesk.com/v1/bpi/currentprice.json')
         .map(res => res.json())
         .subscribe(
-          (data) => {
-            const _lineChartData: Array<any> = new Array(val.value + 1);
-            arrayValues.push(parseFloat(data.bpi.USD.rate.replace(",", "")));
-            const date = new Date();
-            _lineChartData[0] = {data: new Array(val.value + 1), label: "BitCoin/USD  " + date.toDateString()};
-            for (let i = 0; i < arrayValues.length; i++) {
-              _lineChartData[0].data[i] = arrayValues[i];
-            }
-            this.lineChartData = _lineChartData;
-
-            this.lineChartLabels.push(date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds());
-
-          }
+        (data) => {
+          this.updateChart(data, val);
+          this.updateBpiValue(data);
+        }
         );
     });
+  }
+
+  private updateChart(data: any, val: any) {
+    const _lineChartData: Array<any> = new Array(val.value + 1);
+    this.arrayValues.push(this.getFloatValue(data));
+    const date = new Date();
+    _lineChartData[0] = { data: new Array(val.value + 1), label: "BitCoin/USD  " + date.toDateString() };
+    for (let i = 0; i < this.arrayValues.length; i++) {
+      _lineChartData[0].data[i] = this.arrayValues[i];
+    }
+    this.lineChartData = _lineChartData;
+
+    this.lineChartLabels.push(date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds());
+  }
+
+  private updateBpiValue(data:any){
+    this.currentValue = this.getFloatValue(data);
+    var lastValue = this.arrayValues[this.arrayValues.length-2];
+    var change = this.currentValue/lastValue;
+    if (change!==1){
+      this.variation = change;
+    }
+  }
+
+  private getFloatValue(data:any){
+    return parseFloat(data.bpi.USD.rate.replace(",", ""))
   }
 
   public lineChartColors: Array<any> = [
