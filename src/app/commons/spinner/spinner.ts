36dd3ad59d8e90b28adea2a7b042b84bbfa016d8
Sergio Leal <sergio@sergiolealdev.com>
2017-10-30 21:19:46 +0000

    deleting spinner

--- src/app/commons/spinner/spinner.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-spinner',
-  template: `<div></div>`,
-  styleUrls: ['./spinner.css'],
-})
-export class SpinnerComponent {}
-
